0f6400936eb17e31c79cc5e4d990519b97b9dba6
Catherine Estrada <catheestrada.r@gmail.com>
2020-08-25 18:17:08 -0500

    Repo de javaScrip

---
+++ assets/script/script.js
@@ -0,0 +1,38 @@
+var gender = document.getElementById('gender');
+var born = document.getElementById('born');
+var name = document.getElementById('name');
+var song = document.getElementById('song');
+var datos = document.getElementById('datos')
+
+datos.addEventListener('click', () => {
+    var genero = prompt('Género musical')
+
+    gender.textContent = genero
+
+    var año = prompt('Año de Creación')
+    born.innerText = año
+
+    var album = prompt('Primer Album')
+    name.innerText = album
+
+    var cancion = prompt('Primera canción conocida por ti')
+    song.innerText = cancion
+
+})
+/* function datos() {
+    var genero = prompt('Género musical')
+
+    gender.textContent = genero
+
+    var año = prompt('Año de Creación')
+    born.innerText = año
+
+    var album = prompt('Primer Album')
+    name.innerText = album
+
+    var cancion = prompt('Primera canción conocida por ti')
+    song.innerText = cancion
+
+
+
+} */
